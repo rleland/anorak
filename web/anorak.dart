2d1ca7d64b1cf63f2c1185222fec8445f67024c9
Robert Leland <rleland@gmail.com>
2014-10-26 19:02:42 +0100

    move web-related utils out to their own file

--- web/anorak.dart
+++ web/anorak.dart
@@ -1,24 +1,7 @@
 import 'dart:collection';
 import 'dart:html';
 import 'dart:async';
-
-void clearElement(Element e) {
-  while (e.hasChildNodes()) {
-    e.firstChild.remove();
-  }
-}
-
-void debug(String s) {
-  Element dbg = querySelector('#debug');
-  List<Node> children = new List<Node>();
-  children.add(new Text(s));
-  children.add(new Element.br());
-  children.addAll(dbg.childNodes);
-  clearElement(dbg);
-  for (Node child in children) {
-    dbg.append(child);
-  }
-}
+import 'util.dart';
 
 class Pos {
   final int row;
