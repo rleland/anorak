dc38be7b9ccf6fce02e334924a7b5427987c76ab
Robert Leland <rleland@gmail.com>
2014-10-26 19:13:23 +0100

    move util.dart functionality to rendering.dart which will contain rendering logic, this may be restructured later when the rest of the structure is more clear. also for now rendering.dart does not know what a Tile is so this is partly broken.

--- web/anorak.dart
+++ web/anorak.dart
@@ -1,7 +1,7 @@
 import 'dart:collection';
 import 'dart:html';
 import 'dart:async';
-import 'util.dart';
+import 'rendering.dart';
 
 class Pos {
   final int row;
@@ -16,47 +16,37 @@ class Pos {
 }
 
 abstract class Tile {
-  String _explanation;  // Explains what the symbol means.
-  String _symbol;  // Symbol used to render the tile.
-  String _color;  // Color of the symbol.
+  String get explanation;  // Explains what the symbol means.
+  String get symbol;  // Symbol used to render the tile.
+  String get color;  // Color of the symbol.
   bool get passable => true;  // True if player can move onto tile.
   bool get has_event => false;  // True if interacting with the tile results in event.
   bool get bold => false;  // Rendered in bold if true (typically PC and NPCs).
-
-  Element makeElement() {
-    Element span = new Element.span();
-    span.style.setProperty('color', _color);
-    if (bold) {
-      span.style.setProperty('font-weight', 'bold');
-    }
-    span.appendText(_symbol);
-    return span;
-  }
 }
 
 class Grass extends Tile {
-  String _explanation = 'grass';
-  String _symbol = '.';
-  String _color = 'lightgreen';  // TODO: Pick right shade of green.
+  String get explanation => 'grass';
+  String get symbol =>'.';
+  String get color => 'lightgreen';  // TODO: Pick right shade of green.
 }
 
 class Tree extends Tile {
-  String _explanation = 'tree';
-  String _symbol = '#';
-  String _color = 'green';
+  String get explanation => 'tree';
+  String get symbol => '#';
+  String get color => 'green';
   bool get passable => false;
 }
 
 class Path extends Tile {
-  String _explanation = 'path';
-  String _symbol = '#';
-  String _color = 'brown';
+  String get explanation => 'path';
+  String get symbol => '#';
+  String get color => 'brown';
 }
 
 class PlayerTile extends Tile {
-  String _explanation = 'you';
-  String _symbol = '@';
-  String _color = 'black';
+  String get explanation => 'you';
+  String get symbol => '@';
+  String get color => 'black';
   bool get passable => false;
   bool get bold => true;
 }
@@ -122,7 +112,7 @@ class Level {  // Better name, e.g. zone, scene, map, area, etc
           if (!_layers[i].hasTile(pos)) {
             continue;
           }
-          outer.append(_layers[i].tileAt(pos).makeElement());
+          outer.append(renderTile(_layers[i].tileAt(pos)));
           break;  // Only append one per row,col.
         }
       }
--- web/rendering.dart
+++ web/rendering.dart
@@ -17,3 +17,13 @@ void debug(String s) {
     dbg.append(child);
   }
 }
+
+Element renderTile(Tile tile) {
+  Element span = new Element.span();
+  span.style.setProperty('color', tile.color);
+  if (tile.bold) {
+    span.style.setProperty('font-weight', 'bold');
+  }
+  span.appendText(tile.symbol);
+  return span;
+}
