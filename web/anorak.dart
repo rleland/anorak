e5c29f8a124827cfb428528ca16071ff5f4171b4
Robert Leland <rleland@gmail.com>
2014-10-26 20:25:02 +0100

    move input out to own package, but it will need some further cleaning up to remove html logic

--- web/anorak.dart
+++ web/anorak.dart
@@ -1,102 +1,10 @@
 import 'dart:collection';
 import 'dart:html';
 import 'dart:async';
+import 'package:anorak/input.dart';
 import 'package:anorak/level.dart';
 import 'package:anorak/tile.dart';
 import 'rendering.dart';
-
-class Key {
-  static const Key DOWN = const Key(40);
-  static const Key UP = const Key(38);
-  static const Key LEFT = const Key(37);
-  static const Key RIGHT = const Key(39);
-
-  static Key get(int keyCode) {
-     if (_keys.isEmpty) {
-       _initKeys();
-     }
-     return _keys.containsKey(keyCode) ? _keys[keyCode] : null;
-   }
-
-  static final HashMap<int, Key> _keys = new HashMap<int, Key>();
-
-  static void _initKeys() {
-    for (Key k in [DOWN, UP, LEFT, RIGHT]) {
-      _keys[k._code] = k;
-    }
-  }
-
-  final int _code;
-  const Key(int this._code);
-}
-
-class KeyboardListener {
-  static final REPETITION_PERIOD_MS = 200;
-
-  final Queue<Key> _key_queue = new Queue<Key>();
-  final HashMap<Key, int> _held_keys = {};
-  int _last_repetition_check_ms = 0;
-
-  void listen(Window w) {
-    w.onKeyDown.listen(_processKeyDown);
-    w.onKeyUp.listen(_processKeyUp);
-  }
-
-  bool hasKeysToProcess(DateTime now) {
-    _addRepeatsToQueue(now);
-    return _key_queue.isNotEmpty;
-  }
-
-  Key consumeKeyFromQueue() {
-    return _key_queue.removeFirst();
-  }
-
-  void _addRepeatsToQueue(DateTime now) {
-    if (now.millisecondsSinceEpoch - _last_repetition_check_ms < REPETITION_PERIOD_MS ||
-        _held_keys.isEmpty) {
-      return;
-    }
-    _last_repetition_check_ms = now.millisecondsSinceEpoch;
-
-    Key key = null;
-    int max_ts = 0;
-    for (Key k in _held_keys.keys) {
-      if (_held_keys[k] <= max_ts) {
-        continue;
-      }
-      max_ts = _held_keys[k];
-      key = k;
-    }
-    assert(key != null);
-    if (!_key_queue.contains(key)) {
-      _key_queue.add(key);
-    }
-  }
-
-  void _processKeyDown(KeyboardEvent e) {
-    Key key = Key.get(e.keyCode);
-    if (key == null) {
-      return;
-    }
-    _held_keys.putIfAbsent(key, () => e.timeStamp);
-    _key_queue.add(key);
-  }
-
-  void _processKeyUp(KeyboardEvent e) {
-    Key key = Key.get(e.keyCode);
-    if (key != null) {
-      this._held_keys.remove(key);
-    }
-  }
-}
-
-class InputHandler {  // TODO: Rename to describe the type of inputhandler and maybe generic class?
-  static final List<Key> DIRECTION_KEYS = [Key.UP, Key.DOWN, Key.RIGHT, Key.LEFT];
-
-  bool IsDirectionKey(Key key) {
-    return DIRECTION_KEYS.contains(key);
-  }
-}
 
 class Player {
   // TODO: One problems. Doesn't react quickly enough to keypress.
