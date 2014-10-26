36488a676c8f46bee8684c46eca3422a7cccb66f
Robert Leland <rleland@gmail.com>
2014-10-26 23:06:27 +0100

    move rendering back into anorak.dart and encapsulate redraw function in gameloop

--- web/anorak.dart
+++ web/anorak.dart
@@ -8,6 +8,24 @@ import 'package:anorak/level.dart';
 import 'package:anorak/tile.dart';
 import 'rendering.dart';
 
+void clearElement(Element e) {
+  while (e.hasChildNodes()) {
+    e.firstChild.remove();
+  }
+}
+
+void debug(String s) {
+  Element dbg = querySelector('#debug');
+  List<Node> children = new List<Node>();
+  children.add(new Text(s));
+  children.add(new Element.br());
+  children.addAll(dbg.childNodes);
+  clearElement(dbg);
+  for (Node child in children) {
+    dbg.append(child);
+  }
+}
+
 class FpsCounter {
   static final int WINDOW = 5;
   final Element _element;
@@ -62,8 +80,45 @@ class GameLoop {
     _fps_counter.update(now);
     if (_game.loop(now)) {
       // Need redraw.
-      redraw(_game.level);
+      _redraw(_game.level);
     }
+  }
+
+  void _redraw(Level level) {
+    LevelRendererImpl renderer = new LevelRendererImpl();
+    level.render(renderer);
+
+    Element world = querySelector('#world');
+    clearElement(world);
+    world.append(renderer.rendered);
+  }
+}
+
+class LevelRendererImpl implements LevelRenderer {
+  final Element _outer;
+
+  LevelRendererImpl() : _outer = new Element.div() {
+    _outer.style.setProperty('font-family', 'monospace');
+  }
+
+  void AddTile(Tile tile) {
+    _outer.append(_renderTile(tile));
+  }
+
+  void NewRow() {
+    _outer.append(new Element.br());
+  }
+
+  Element get rendered => _outer;
+
+  Element _renderTile(Tile tile) {
+    Element span = new Element.span();
+    span.style.setProperty('color', tile.color);
+    if (tile.bold) {
+      span.style.setProperty('font-weight', 'bold');
+    }
+    span.appendText(tile.symbol);
+    return span;
   }
 }
 
