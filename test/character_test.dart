472e74a8d369d1b25493a894641e5c2f44f977cf
Robert Leland <rleland@gmail.com>
2014-10-29 22:23:17 +0100

    test for capMagnitude

---
+++ test/character_test.dart
@@ -0,0 +1,16 @@
+library character_test;
+
+import 'package:anorak/mob.dart';
+import 'package:unittest/unittest.dart';
+
+main() {
+  group("test loose functions", () {
+    test("test cap", () {
+      expect(capMagnitude(5, 1), equals(1));
+      expect(capMagnitude(-5, 1), equals(-1));
+      expect(capMagnitude(7, 2), equals(2));
+      expect(capMagnitude(-7, 2), equals(-2));
+      expect(capMagnitude(0, 3), equals(0));
+    });
+  });
+}
