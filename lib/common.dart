07035075b615687064eaf9ca2baf614a920fdda6
Robert Leland <rleland@gmail.com>
2014-11-01 14:37:12 +0100

    add a poslist to make life and code a bit simpler

--- lib/common.dart
+++ lib/common.dart
@@ -1,5 +1,7 @@
 library common;
 
+import "dart:collection";
+
 // Note: This looks quite similar to dart point. Consider using that if this doesn't evolve.
 class Pos {
   static const Pos MOVE_DOWN = const Pos(1, 0);
@@ -20,6 +22,27 @@ class Pos {
   }
   String toString() {
     return "$row,$col";
+  }
+}
+
+class PosList<T> {
+  final int _rows;
+  final int _cols;
+  final List<T> _list;
+
+  int get length => _list.length;
+
+  PosList(int rows, int cols)
+      : _rows = rows, _cols = cols, _list = new List<T>(rows * cols);
+
+  T operator[](Pos p) {
+    assert(p.row < _rows && p.col < _cols);
+    return _list[p.row * _cols + p.col];
+  }
+
+  void operator[]=(Pos p, T t) {
+    assert(p.row < _rows && p.col < _cols);
+    _list[p.row * _cols + p.col] = t;
   }
 }
 
