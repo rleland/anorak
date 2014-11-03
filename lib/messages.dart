161e8862eec20f77b6fac9844f56e2df23bc40a3
Robert Leland <rleland@gmail.com>
2014-11-03 21:52:05 +0100

    consistency on fullstops

--- lib/messages.dart
+++ lib/messages.dart
@@ -2,7 +2,7 @@ library messages;
 
 class Messages {
   static String Damage(String attacker, String defender, int damage) {
-    return "$attacker hits $defender for $damage";
+    return "$attacker hits $defender for $damage.";
   }
 
   static String AttackMiss(String attacker, String defender) {
