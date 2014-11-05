a61c9be0bd5ce1bc59bb4d7954dd3225f4c9a627
Robert Leland <rleland@gmail.com>
2014-11-05 09:50:36 +0100

    add message output to events

--- lib/events.dart
+++ lib/events.dart
@@ -1,5 +1,7 @@
 library events;
 
+import 'package:anorak/common.dart';
+import 'package:anorak/messages.dart';
 import 'package:anorak/mob.dart';
 
 abstract class Event {
@@ -9,17 +11,19 @@ abstract class Event {
 }
 
 abstract class MobEvent extends Event {
-  void Process(Mob mob);
+  void Process(MessageLog log, Mob mob);
 
   int get event_type => Event.TYPE_MOB;
 }
 
 class Damage extends MobEvent {
+  final String _name;
   final int _damage;
 
-  Damage(int this._damage);
+  Damage(String this._name, int this._damage);
 
-  void Process(Mob mob) {
+  void Process(MessageLog log, Mob mob) {
     mob.stats.hp -= _damage;
+    log.write(Messages.Damage(_name, mob.name, _damage));
   }
 }
