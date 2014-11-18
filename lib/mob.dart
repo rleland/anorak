1fd3b96d8b0c88d5677af66b77dd9f73340bacb0
Robert Leland <rleland@gmail.com>
2014-11-18 18:56:45 +0100

    start defining framework for skills and managing attacks

--- lib/mob.dart
+++ lib/mob.dart
@@ -6,6 +6,30 @@ import "package:anorak/path_finding.dart";
 import "package:anorak/tile.dart";
 
 
+abstract class Skill {
+  // True if it can only be activated on yourself and thus is not targeted or ranged.
+  bool get self_skill;
+  // False if it can only target adjacent (non-diagonal) enemies.
+  bool get ranged;
+  // True if the skill can be activated.
+  bool ready(DateTime now);
+}
+
+abstract class AttackSkill extends Skill {
+  // By defintiion you only attack your enemies, not yourself.
+  bool get self_skill => false;
+}
+
+class BasicAttack extends AttackSkill {
+  static const BASIC_ATTACK_RATE_MS = 1000;
+
+  final _rate = new RateLimiter(BASIC_ATTACK_RATE_MS);
+
+  bool get ranged => false;
+  // TODO: Pretty clear we need to be able to 'peek' at the rate.
+  bool ready(DateTime now) => _rate.checkRate(now, peek: true);
+}
+
 abstract class Mob {
   final _buffs = new BuffContainer();
   Pos _pos;
@@ -48,13 +72,17 @@ abstract class Npc extends Mob {
     return calculateMove(game_state);
   }
 
-
   bool shouldMove(DateTime now, GameState game_state) {
     return hasAggro(game_state) && _move_rate.checkRate(now);
   }
 
+  bool shouldAttack(DateTime now, GameState game_state) {
+    return hasAggro(game_state) && hasViableAttack(now, game_state);
+  }
+
   Pos calculateMove(GameState game_state);
   bool hasAggro(GameState game_state);
+  bool hasViableAttack(DateTime now, GameState game_state);
 }
 
 class Rat extends Npc {
@@ -79,5 +107,9 @@ class Rat extends Npc {
 
   bool hasAggro(GameState game_state) {
     return inRange(_pos, game_state.player_pos, ROW_AGGRO, COL_AGGRO);
+  }
+
+  bool hasViableAttack(DateTime now, GameState game_state) {
+    return true;
   }
 }
