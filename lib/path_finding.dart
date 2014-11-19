ce681698cdb1d6360f81fc484dc60a252cd01fa1
Robert Leland <rleland@gmail.com>
2014-11-19 16:20:16 +0100

    line of sight implementation that only works for very specific start/end points. generalization and tests come later

--- lib/path_finding.dart
+++ lib/path_finding.dart
@@ -19,6 +19,49 @@ Pos moveCloser(Pos from, Pos to) {
 }
 
 bool inRange(Pos src, Pos dst, int row_dist, int col_dist) {
-  Pos diff = dst - src;
-  return diff.row.abs() <= row_dist && diff.col.abs() <= col_dist;
+  Pos delta = dst - src;
+  return delta.row.abs() <= row_dist && delta.col.abs() <= col_dist;
+}
+
+// Bresenham for lines in the first octant.
+List<Pos> _firstOctetBresenham(Pos from, Pos to) {
+  // In mapping x,y coordinates in Bresenham to the row/col matrix this convention is used:
+  // x : col
+  // y : row
+  // points are (x, y) i.e (col, row)
+  assert(to.row > from.row);
+  assert(to.col > from.col);
+  Pos delta = to - from;
+  assert(delta.col <= delta.row);
+
+  List<Pos> plot;
+  int y = from.row;
+  int e = 0;
+
+  for (int x = from.col; x <= to.col; ++x) {
+    plot.add(new Pos(y, x));
+    e += delta.row;
+    if (2 * e >= delta.col) {
+      ++y;
+      e -= delta.col;
+    }
+  }
+  return plot;
+}
+
+List<Pos> drawLine(Pos from, Pos to) {
+  // Use bresenham for this and return the positions the line passes through.
+  // This will have the caveat that line of sight doesn't necessarily mean the two can be reached.
+  // E.g. the line between X and Y marked by o's is:
+  // ..Xooo........
+  // ......oooY....
+  // Where there is a diagonal 'jump', rather than overlap.
+  // It's possible Bresenham can be modified to include this overlap if necessary, or maybe one of
+  // the anti-aliasing algorithms can be adapted, but for a first implementation this looks fine.
+  return _firstOctetBresenham(from, to);
+}
+
+bool hasLineOfSight(Pos from, Pos to, GameState game_state) {
+  List<Pos> line = drawLine(from, to);
+  return line.every(game_state.isPassable);
 }
