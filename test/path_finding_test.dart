a19540b8c08bc826384f9e5979f39c26d5b521f5
Robert Leland <rleland@gmail.com>
2014-11-20 20:42:46 +0100

    oops accidentally submitted commented out code

--- test/path_finding_test.dart
+++ test/path_finding_test.dart
@@ -25,20 +25,6 @@ main() {
 
   // TODO: Test: Straight lines, other octants.
   group("line drawing test", () {
-    /*test("get octant test", () {
-      expect(getOctant(new Pos(5, 5), new Pos(6, 7)), equals(0));
-      expect(getOctant(new Pos(5, 5), new Pos(7, 7)), equals(0));
-      expect(getOctant(new Pos(5, 5), new Pos(8, 7)), equals(1));
-      expect(getOctant(new Pos(5, 5), new Pos(8, 3)), equals(2));
-      expect(getOctant(new Pos(5, 5), new Pos(8, 2)), equals(3));
-      expect(getOctant(new Pos(5, 5), new Pos(6, 2)), equals(3));
-      expect(getOctant(new Pos(5, 5), new Pos(4, 2)), equals(4));
-      expect(getOctant(new Pos(5, 5), new Pos(2, 2)), equals(4));
-      expect(getOctant(new Pos(5, 5), new Pos(1, 2)), equals(5));
-      expect(getOctant(new Pos(5, 5), new Pos(1, 6)), equals(6));
-      expect(getOctant(new Pos(5, 5), new Pos(4, 6)), equals(7));
-      expect(getOctant(new Pos(5, 5), new Pos(4, 8)), equals(7));
-    });*/
     test("first octant test", () {
       expect(drawLine(new Pos(0, 0), new Pos(1, 3)),
              equals([new Pos(0,0), new Pos(0,1), new Pos(1,2), new Pos(1,3)]));
