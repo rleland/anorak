f14474588924425f512ac2ae7b8a0b00dd1e956b
Robert Leland <rleland@gmail.com>
2014-11-20 20:42:24 +0100

    refactor the algorithm to work around the origin, do the right mapping of points, and update some docommentation

--- lib/path_finding.dart
+++ lib/path_finding.dart
@@ -23,34 +23,32 @@ bool inRange(Pos src, Pos dst, int row_dist, int col_dist) {
   return delta.row.abs() <= row_dist && delta.col.abs() <= col_dist;
 }
 
-// Bresenham for lines in the first octant.
-List<Pos> _firstOctantBresenham(Pos from, Pos to) {
+// Bresenham for lines in the first octant, assuming the line starts in the origin.
+List<Pos> _firstOctantBresenham(Pos to) {
   // In mapping x,y coordinates in Bresenham to the row/col matrix this convention is used:
   // x : col
   // y : row
   // points are (x, y) i.e (col, row)
-  assert(to.row > from.row);
-  assert(to.col > from.col);
-  Pos delta = to - from;
-  assert(delta.col >= delta.row);
+  assert(to.row > 0);
+  assert(to.col > 0);
+  assert(to.col >= to.row);
 
   List<Pos> plot = [];
-  int y = from.row;
+  int y = 0;
   int e = 0;
 
-  for (int x = from.col; x <= to.col; ++x) {
+  for (int x = 0; x <= to.col; ++x) {
     plot.add(new Pos(y, x));
-    e += delta.row;
-    if (2 * e >= delta.col) {
+    e += to.row;
+    if (2 * e >= to.col) {
       ++y;
-      e -= delta.col;
+      e -= to.col;
     }
   }
   return plot;
 }
 
-// Gets which octant the line lies in. Octants can be thought of as classifying the direction of the
-// line with the 'from' point as the origin
+// Gets which octant the line lies in, assuming it starts at the origin.
 // Octants:
 //  \2|1/
 //  3\|/0
@@ -61,40 +59,55 @@ List<Pos> _firstOctantBresenham(Pos from, Pos to) {
 // right. Note that this is different from the coordinate system used for the map which has the
 // origin in the top left, and y (rows) increasing going down from the origin. However for this
 // purpose that's not important.
-int _getOctant(Pos from, Pos to) {
-  Pos diff = to - from;
-  assert(diff.row != 0 && diff.col != 0);
-  if (diff.row > 0 && diff.col > 0) {
-    return diff.col >= diff.row ? 0 : 1;
-  } else if (diff.row > 0 && diff.col < 0) {
-    return diff.col.abs() >= diff.row ? 3 : 2;
-  } else if (diff.row < 0 && diff.col < 0) {
-    return diff.col <= diff.row ? 4 : 5;
+int _getOctant(Pos to) {
+  assert(to.row != 0 && to.col != 0);
+  if (to.row > 0 && to.col > 0) {
+    return to.col >= to.row ? 0 : 1;
+  } else if (to.row > 0 && to.col < 0) {
+    return to.col.abs() >= to.row ? 3 : 2;
+  } else if (to.row < 0 && to.col < 0) {
+    return to.col <= to.row ? 4 : 5;
   } else {  // diff.row < 0 && diff.col > 0
-    return diff.col >= diff.row.abs() ? 7: 6;
+    return to.col >= to.row.abs() ? 7: 6;
   }
 }
 
-List<Pos> drawLine(Pos from, Pos to) {
-  // Use bresenham for this and return the positions the line passes through.
-  // This will have the caveat that line of sight doesn't necessarily mean the two can be reached.
-  // E.g. the line between X and Y marked by o's is:
-  // ..Xooo........
-  // ......oooY....
-  // Where there is a diagonal 'jump', rather than overlap.
-  // It's possible Bresenham can be modified to include this overlap if necessary, or maybe one of
-  // the anti-aliasing algorithms can be adapted, but for a first implementation this looks fine.
-  int octant = _getOctant(from, to);
+// Maps the pos to or from the first octant.
+Pos _mapToOctant(Pos pos, int octant, {bool undo: false}) {
   switch (octant) {
-    case 0:
-      return _firstOctantBresenham(from, to);
-    default:
-      throw new UnimplementedError("Octant not supported: $octant");
+    case 0: return pos;
+    case 1: return new Pos(pos.col, pos.row);
+    case 2: return undo ? new Pos(pos.col, -pos.row) : new Pos(-pos.col, pos.row);
+    case 3: return new Pos(pos.row, -pos.col);
+    case 4: return new Pos(-pos.row, -pos.col);
+    case 5: return new Pos(-pos.col, -pos.row);
+    case 6: return undo ? new Pos(-pos.col, pos.row) : new Pos(pos.col, -pos.row);
+    case 7: return new Pos(-pos.row, pos.col);
+    default: throw new UnimplementedError("Unsupported octant: $octant");
   }
-
 }
 
+// Returns the positions, inclusive of from and to, needed to be filled to draw a line between the
+// two points.
+Iterable<Pos> drawLine(Pos from, Pos to) {
+  // Shift the coordinate system so you're finding the line from the origin to the delta. This makes
+  // dealing with octants and mapping between them more conceptually straight forward.
+  Pos delta = to - from;
+  int octant = _getOctant(delta);
+  Pos mapped_delta = _mapToOctant(delta, octant);
+  return _firstOctantBresenham(mapped_delta)
+      .map((p) => _mapToOctant(p, octant, undo: true))
+      .map((p) => p + from);
+}
+
+// Uses bresenham line drawing to determine tiles which need to be free to establish line of sight.
+// This will have the caveat that line of sight doesn't necessarily mean the two can be reached.
+// E.g. the line between X and Y marked by o's is:
+// ..Xooo........
+// ......oooY....
+// Where there is a diagonal 'jump', rather than overlap.
+// It's possible Bresenham can be modified to include this overlap if necessary, or maybe one of
+// the anti-aliasing algorithms can be adapted, but for a first implementation this looks fine.
 bool hasLineOfSight(Pos from, Pos to, GameState game_state) {
-  List<Pos> line = drawLine(from, to);
-  return line.every(game_state.isPassable);
+  return drawLine(from, to).every(game_state.isPassable);
 }
