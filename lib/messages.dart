330d5ed517104a4e49720fcf2b0a491e8a71ac89
Robert Leland <rleland@gmail.com>
2014-11-21 11:11:34 +0100

    use => for simple returns in messages library

--- lib/messages.dart
+++ lib/messages.dart
@@ -1,27 +1,11 @@
 library messages;
 
 class Messages {
-  static String Damage(String attacker, String defender, int damage) {
-    return "$attacker hits $defender for $damage.";
-  }
-
-  static String AttackMiss(String attacker, String defender) {
-    return "$attacker misses $defender.";
-  }
-
-  static String Dead(String name) {
-    return "$name was killed.";
-  }
-
-  static String LevelUp(String name, int level) {
-    return "$name levels up to level $level!";
-  }
-
-  static String BurnAppliedPassive(String name) {
-    return "$name caught fire.";
-  }
-
-  static String BurnBuff(String name, int damage) {
-    return "$name was burned for $damage damage.";
-  }
+  static String Damage(String attacker, String defender, int damage) =>
+      "$attacker hits $defender for $damage.";
+  static String AttackMiss(String attacker, String defender) => "$attacker misses $defender.";
+  static String Dead(String name) => "$name was killed.";
+  static String LevelUp(String name, int level) => "$name levels up to level $level!";
+  static String BurnAppliedPassive(String name) => "$name caught fire.";
+  static String BurnBuff(String name, int damage) => "$name was burned for $damage damage.";
 }
